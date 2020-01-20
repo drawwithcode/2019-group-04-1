417b6fc635c3191a55fc725b1e2f0359b71c8df1
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2020-01-20 19:27:41 +0100

    Create sketch landing page.js

---
+++ sketch landing page.js
@@ -0,0 +1,34 @@
+var canvas;
+var immaginesfondo;
+var logo;
+
+function preload(){
+  immaginesfondo = loadImage("mappamundo.jpeg");
+  logo = loadImage("logo.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+  canvas.parent('#tela');
+
+  imageMode(CENTER);
+  image(immaginesfondo, width/1.5, height/2, immaginesfondo.width*1.3, immaginesfondo.height*1.3);
+
+  for(var x = 0; x < windowWidth; x += 10) {
+    for(var y = 0; y < windowHeight; y += 10) {
+      noStroke();
+      fill(26, 24, 56);
+      ellipse(x, y, 2);
+    }
+  }
+
+  //image(logo, width/2, height/2, logo.width/10, logo.height/10);
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
