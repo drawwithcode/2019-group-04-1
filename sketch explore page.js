791f42e551f0b57611fea65bf57a0aefcaeb49c3
Giorgio Ventura <ventura.giorgio.95@gmail.com>
2020-01-23 15:54:04 +0100

    Update sketch explore page.js

--- sketch explore page.js
+++ sketch explore page.js
@@ -6,6 +6,8 @@ var lafontreg;
 var lafontbold;
 var i = 4;
 
+var cerchio = select('.circle2');
+
 function preload(){
   immaginesfondo = loadImage("mappamundo.jpeg");
   logo = loadImage("logo.png");
@@ -18,13 +20,12 @@ function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
   canvas.parent('#tela');
 
+  frameRate(100);
   angleMode(DEGREES);
 }
 
 function draw() {
 
-  imageMode(CENTER);
-  image(immaginesfondo, width/1.8, height/2, immaginesfondo.width*1.3, immaginesfondo.height*1.3);
   push();
   for(var x = 0; x < windowWidth; x += 10) {
     for(var y = 0; y < windowHeight; y += 10) {
@@ -37,46 +38,12 @@ function draw() {
   pop();
 
   push();
-  translate(0,height/2);
-  ellipseMode(CENTER);
-  noFill();
-  strokeWeight(4);
-  stroke('white');
-  ellipse(0,0, height*1.2);
+
+  cerchio.style('display','none');
+
   pop();
 
-  push();
-  translate(400, 200);
-  textSize(30);
-  textFont(lafontreg);
-  fill('white');
-  text('COLOMBIA', 30, 10);
-  rotate(frameCount*i);
-  imageMode(CENTER);
-  image(icon, 0, 0, 30, 30);
-  pop();
 
-  push();
-  translate(410, 260);
-  textSize(30);
-  textFont(lafontreg);
-  fill('white');
-  text('CHILE', 30, 10);
-  rotate(frameCount*i);
-  imageMode(CENTER);
-  image(icon, 0, 0, 30, 30);
-  pop();
-
-  push();
-  translate(420, 320);
-  textSize(30);
-  textFont(lafontreg);
-  fill('white');
-  text('HONG KONG', 30, 10);
-  rotate(frameCount*i);
-  imageMode(CENTER);
-  image(icon, 0, 0, 30, 30);
-  pop();
 }
 
 
