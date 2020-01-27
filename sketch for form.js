ea1d554bfaf4791f13fe9d6c64c66b90c930df0a
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2020-01-27 13:44:50 +0100

    Create sketch for form.js

---
+++ sketch for form.js
@@ -0,0 +1,18 @@
+var canvas;
+
+
+function preload(){
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+  canvas.parent("#tela");s
+}
+
+function draw() {
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
