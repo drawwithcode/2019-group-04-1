a7dfc68b9feec726d2032792dd93870a43f2d307
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2020-01-30 00:12:59 +0100

    Update sketch for form.js

--- sketch for form.js
+++ sketch for form.js
@@ -1,14 +1,32 @@
 var canvas;
 var data;
+var nations;
+var states;
+var cities;
+var dropdown;
+var allthenames = [];
 
 function preload(){
   data = loadJSON("world-cities.json");
-
+  nations = loadJSON("countries.json");
+  states = loadJSON("states.json");
+  cities = loadJSON("cities.json");
 }
 
 function setup() {
   canvas = createCanvas(windowWidth,windowHeight);
   canvas.parent("#tela");
+
+//select all elements with class "name" inside the .json file
+  for(var i = 0; i < nations.countries.length; i++){
+    allthenames = nations.countries[i].name; // e seleziona solo l'ultimo stato porcoddio
+  }
+  console.log(allthenames);
+
+  dropdown = createSelect();
+  dropdown.position(10, 10);
+  dropdown.option(thename);
+
 }
 
 function draw() {
