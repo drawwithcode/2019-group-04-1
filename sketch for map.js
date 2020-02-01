0a37ec528e650d075f038cfa08ac25815d659342
itsmichaelsas <55444847+itsmichaelsas@users.noreply.github.com>
2020-02-01 17:16:58 +0100

    Update sketch for map.js

--- sketch for map.js
+++ sketch for map.js
@@ -1,6 +1,10 @@
 var canvas;
+var canvas2;
 var geomap;
 var speak;
+var dropdown1;
+var dropdown2;
+var dropdown3;
 
 function preload(){
   speak = loadImage("speaker.png");
@@ -30,8 +34,18 @@ function setup() {
       ellipse(x, y, 2);
     }
   }
-
   image(speak,20,height-50,30,30);
+
+dropdown1.parent('#form');
+dropdown2.parent('#form');
+dropdown3.parent('#form');
+
+  dropdown1 = createSelect();
+  dropdown1.position(10, 10);
+  dropdown2 = createSelect();
+  dropdown2.position(10, 40);
+  dropdown3 = createSelect();
+  dropdown3.position(10, 70);
 }
 
 function draw() {
